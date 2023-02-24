b16bb32ff0f337d2672cec27ad5eaac3644e7016
WeiChin <weichin0417@gmail.com>
2023-02-25 01:47:53 +0800

    user bottom navigation

--- app/src/main/java/com/example/easyjob/user/UserHome.kt
+++ app/src/main/java/com/example/easyjob/user/UserHome.kt
@@ -4,46 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.easyjob.MainActivity
 import com.example.easyjob.databinding.ActivityUserHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
 class UserHome : AppCompatActivity() {
     private lateinit var binding: ActivityUserHomeBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var dbref: DatabaseReference
-    private lateinit var uid: String
-    private lateinit var user: UserData
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        auth = FirebaseAuth.getInstance()
-        dbref = FirebaseDatabase.getInstance().getReference("Users")
-        uid = auth.currentUser?.uid.toString()
+        val fragmentId = binding.fragmentContainerView2.id
+        val navHostFragment = supportFragmentManager.findFragmentById(fragmentId) as NavHostFragment
 
-        dbref.child(uid).addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot){
-                user = snapshot.getValue(UserData::class.java)!!
-                binding.tvt.text = user.email
-            }
+        val nv = findViewById<BottomNavigationView>(binding.btmNvUser.id)
 
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-        })
+        navController = navHostFragment.navController
 
-        binding.button.setOnClickListener {
-            auth.signOut()
-            Intent(this, MainActivity::class.java).also{
-                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(it)
-                Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
+        nv.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/easyjob/user/UserHomeFragment.kt
+++ app/src/main/java/com/example/easyjob/user/UserHomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.easyjob.employer
+package com.example.easyjob.user
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,57 +6,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.easyjob.MainActivity
 import com.example.easyjob.R
-import com.example.easyjob.databinding.FragmentEmployerHomeBinding
+import com.example.easyjob.databinding.FragmentUserHomeBinding
+import com.example.easyjob.employer.PostJob
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
-class EmployerHomeFragment : Fragment() {
+class UserHomeFragment : Fragment() {
 
-    private var _binding: FragmentEmployerHomeBinding? =null
+    private var _binding: FragmentUserHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var auth: FirebaseAuth
     private lateinit var dbref: DatabaseReference
     private lateinit var uid: String
-    private lateinit var employer: EmployerData
+    private lateinit var user: UserData
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentEmployerHomeBinding.inflate(inflater,container,false)
+        _binding = FragmentUserHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onResume() {
         super.onResume()
         auth = FirebaseAuth.getInstance()
-        dbref = FirebaseDatabase.getInstance().getReference("Employers")
+        dbref = FirebaseDatabase.getInstance().getReference("Users")
         uid = auth.currentUser?.uid.toString()
 
         dbref.child(uid).addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot){
-                employer = snapshot.getValue(EmployerData::class.java)!!
-                binding.tvtest.text = employer.email
+                user = snapshot.getValue(UserData::class.java)!!
+                binding.tvt.text = user.email
             }
 
             override fun onCancelled(error: DatabaseError) {}
         })
 
-        binding.button2.setOnClickListener {
+        binding.button.setOnClickListener {
             auth.signOut()
-            activity?.let{
+            activity?.let {
                 val intent = Intent (it, MainActivity::class.java).also {
                     it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 }
-                it.startActivity(intent)
-            }
-        }
-
-        binding.btnPostJob.setOnClickListener{
-            activity?.let{
-                val intent = Intent (it, PostJob::class.java)
                 it.startActivity(intent)
             }
         }
