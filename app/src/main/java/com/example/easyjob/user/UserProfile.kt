b6ddbe93a4a18074813b163e9c37a3a849080486
WeiChin <weichin0417@gmail.com>
2023-02-26 03:19:51 +0800

    user profile ui

--- app/src/main/java/com/example/easyjob/user/UserProfile.kt
+++ app/src/main/java/com/example/easyjob/user/UserProfile.kt
@@ -1,60 +1,63 @@
 package com.example.easyjob.user
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.easyjob.MainActivity
 import com.example.easyjob.R
+import com.example.easyjob.databinding.FragmentUserProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UserProfile.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UserProfile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentUserProfileBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dbref: DatabaseReference
+    private lateinit var uid: String
+    private lateinit var user: UserData
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserProfile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserProfile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onResume() {
+        super.onResume()
+        auth = FirebaseAuth.getInstance()
+        dbref = FirebaseDatabase.getInstance().getReference("Users")
+        uid = auth.currentUser?.uid.toString()
+
+        dbref.child(uid).addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot){
+                user = snapshot.getValue(UserData::class.java)!!
+                binding.tvUserName.text = user.name
+                binding.tvUserEmail.text = user.email
             }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+//        binding.button.setOnClickListener {
+//            auth.signOut()
+//            activity?.let {
+//                val intent = Intent (it, MainActivity::class.java).also {
+//                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                }
+//                it.startActivity(intent)
+//            }
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
